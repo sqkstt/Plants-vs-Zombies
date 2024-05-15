28dcfe76b806b5c3da1dc65cecb03cfa7754053b
sqkstt <148732692+sqkstt@users.noreply.github.com>
2024-05-15 17:05:25 +0800

    Update AppStart.java

--- Plants vs Zombies/src/AppStart.java
+++ Plants vs Zombies/src/AppStart.java
@@ -3,24 +3,21 @@ import java.awt.*;
 import java.awt.Menu;
 
 public class AppStart extends JLayeredPane {
-    ImageIcon imageIcon;
+    static ImageIcon imageIcon= new ImageIcon("D:\\Lab\\Plants vs Zombies\\resources\\src\\IconImage.png");// 导入图片;
+    static SunLabel sunLabel;
     AppStart() {
         this.setSize(1000,618);
         this.setVisible(true);
-        MyMenu myMenu = new MyMenu();
-        this.add(myMenu, Integer.valueOf(0)); // 添加myMenu到JLayeredPane，并设置深度为0,作用是监听器
-        this.add(myMenu.start, Integer.valueOf(2)); // 添加start到JLayeredPane，并设置深度为2
-        this.add(myMenu.Menu, Integer.valueOf(1)); // 添加Menu到JLayeredPane，并设置深度为1
-        imageIcon = new ImageIcon("D:\\Lab\\Plants vs Zombies\\resources\\src\\IconImage.png");// 导入图片
+        MyMenu myMenu = new MyMenu(this);
     }
     AppStart(boolean a) {
         this.setSize(1256,638);
         this.setVisible(true);
-        Background background = new Background();
-        this.add(background,Integer.valueOf(-1));
-        this.add(background.bg,Integer.valueOf(0));
-        this.add(background.seedBank,Integer.valueOf(1));
-        this.add(background.shovelBank,Integer.valueOf(2));
-        imageIcon = new ImageIcon("D:\\Lab\\Plants vs Zombies\\resources\\src\\IconImage.png");// 导入图片
+        Background background = new Background(this);
+        sunLabel = new SunLabel(this);
+        Sun sun = new Sun(200,200,this);
+    }
+    public static SunLabel getSunLabel() {
+        return sunLabel;
     }
 }
