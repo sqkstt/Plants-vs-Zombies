4e116ac1bb6bd64d45311aca1647961291e4151f
zzy10151020 <166393195+zzy10151020@users.noreply.github.com>
2024-05-16 23:40:32 +0800

    Update AppStart.java

--- Plants vs Zombies/src/AppStart.java
+++ Plants vs Zombies/src/AppStart.java
@@ -15,8 +15,21 @@ public class AppStart extends JLayeredPane {
         Background background = new Background(this);
         sunLabel = new SunLabel(this);
         sunLabel.SunGenerate(this);//自然阳光生成
+
+        Seed seed0 = new Seed(ChangeImageIcon(new ImageIcon("resources\\src\\PeaShooterCard.png"),
+                2f), 100,this);
+        Seed seed1 = new Seed(ChangeImageIcon(new ImageIcon("resources\\src\\SunFlowerCard.png"),
+                2f), 50,this);
     }
+    
     public static SunLabel getSunLabel() {
         return sunLabel;
+    }
+
+    public ImageIcon ChangeImageIcon(ImageIcon picture, float multiple) {
+        Image scaledPicture = picture.getImage().getScaledInstance((int)(picture.getIconWidth()/multiple),
+                (int)(picture.getIconHeight()/multiple), Image.SCALE_SMOOTH);
+        picture = new ImageIcon(scaledPicture);   // 调整图像大小
+        return picture;
     }
 }
